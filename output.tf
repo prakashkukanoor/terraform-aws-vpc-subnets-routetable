674d78c3acf76377978557e5d9bae277f2f74daf
prakashkukanoor <prakashkop054@gmail.com>
2025-03-24 16:33:42 -0400

    vpce gateway endpoint

--- output.tf
+++ output.tf
@@ -15,7 +15,7 @@ output "database_private_subnet_ids" {
 }
 
 output "aws_vpce_gateway_dynamodb_id" {
-  value = aws_vpc_endpoint.gateway
+  value = aws_vpc_endpoint.gateway["dynamodb"]
 }
 
 # output "aws_vpce_gateway_s3_id" {
