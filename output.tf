5db62add0814a02b3ad3305f8a82d9438d6946b2
prakashkukanoor <prakashkop054@gmail.com>
2025-03-24 16:04:37 -0400

    vpce gateway endpoint

--- output.tf
+++ output.tf
@@ -1,16 +1,16 @@
-output "vpc" {
+output "vpc_id" {
   value = aws_vpc.this.id
 }
 
-output "application_public_subnets" {
+output "application_public_subnet_ids" {
   value = aws_subnet.application_public[*].id
 }
 
-output "application_private_subnets" {
+output "application_private_subnet_ids" {
   value = aws_subnet.application_private[*].id
 }
 
-output "database_private_subnets" {
+output "database_private_subnet_ids" {
   value = aws_subnet.database_private[*].id
 }
 
